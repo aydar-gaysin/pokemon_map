fb97a0e30dc832ecbd1abdcd1678169f28d5ed65
aydar.gaysin@gmail.com <4r8hMBYjPh5Gmyk>
2021-05-18 23:26:04 +0300

    Add geo and time parameters for pokemon entity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,6 +3,17 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='', blank=True)
 
     def __str__(self):
         return '{}'.format(self.title)
+
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    appeared_at = models.DateTimeField()
+    disappeared_at = models.DateTimeField()
+
+

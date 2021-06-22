0c95035bdd47032fbfdc2b575b9bb978b1b1d6fa
aydar-gaysin <aydar-gaysin@users.noreply.github.com>
2021-06-22 03:07:49 +0300

    Change to get pokemon data from DB

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,8 +1,10 @@
 import folium
 import json
 
+from pokemon_entities.models import Pokemon, PokemonEntity
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from itertools import count
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -31,20 +33,23 @@ def show_all_pokemons(request):
         pokemons = json.load(database)['pokemons']
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
-            )
 
+    pokemon_entities = PokemonEntity.objects.all()
+    # for pokemon in pokemons:
+    for pokemon_entity in pokemon_entities:
+        add_pokemon(
+            folium_map, pokemon_entity.latitude,
+            pokemon_entity.longitude,
+            pokemon_entity.pokemon.photo.url
+        )
+
+    pokemons_model = Pokemon.objects.all()
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon in pokemons_model:
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': pokemon.photo.url,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={

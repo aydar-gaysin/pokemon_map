1d079804fef55f70ba73372aff5b7eac4a7b2db1
aydar.gaysin@gmail.com <4r8hMBYjPh5Gmyk>
2021-07-09 10:22:05 +0300

    Updated Pokemon model with following relationships “backward”

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,7 +7,10 @@ class Pokemon(models.Model):
     title_jp = models.CharField(max_length=200, blank=True)
     photo = models.ImageField(upload_to='', blank=True)
     description = models.TextField(blank=True)
-    previous_evolution = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
+    previous_evolution = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='next_evolutions'
+    )
 
     def __str__(self):
         return '{}'.format(self.title)

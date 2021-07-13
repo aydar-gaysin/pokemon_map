11ad4fa4cc7e1ed6ed894623ecb2f49eed1f8377
aydar-gaysin <aydar-gaysin@users.noreply.github.com>
2021-07-14 00:13:33 +0300

    Added related_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -16,7 +16,8 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Вид покемона')
+    pokemon = models.ForeignKey(
+        Pokemon, on_delete=models.CASCADE, related_name='pokemon_specie', verbose_name='Вид покемона')
     latitude = models.FloatField(verbose_name='Координаты широты')
     longitude = models.FloatField(verbose_name='Координаты долготы')
     appeared_at = models.DateTimeField(verbose_name='Появляется в')
